b8409fb34ddcb94da4d6fe30f15a98843050db71
Artem Kartasov <agneum@gmail.com>
2017-06-19 21:43:47 +0700

    short-link-go genarator handler

--- main.go
+++ main.go
@@ -20,7 +20,13 @@ CREATE TABLE IF NOT EXISTS links (
   PRIMARY KEY (id), UNIQUE KEY UNIQ_Code (code)
 );`
 
+// type AutoIncr struct {
+//     ID       uint64
+//     CreatedAt  time.Time
+// }
+
 type Link struct {
+	// AutoIncr
 	ID        int     `db:"id"`
 	Code      string  `db:"code"`
 	Link      string  `db:"link"`
@@ -82,6 +88,40 @@ func redir(conn *sqlx.DB) httprouter.Handle {
 
 func generateCode(conn *sqlx.DB) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		var link Link
+		var url string
+		r.ParseForm()
+		for key, values := range r.Form {
+			for _, value := range values {
+				if key == "url" {
+					url = value
+					break
+				}
+			}
+		}
 
+		if url == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Url is required"))
+			return
+		}
+
+		err := conn.Get(&link, "SELECT * FROM links WHERE link = ?", url)
+		if err == sql.ErrNoRows {
+			code := "code"
+			res, err := conn.Exec("INSERT INTO links (code, link, created_at) VALUES (?, ?, NOW())", code, url)
+			if err != nil {
+				panic(err)
+			}
+			id, err := res.LastInsertId()
+			if err != nil {
+				panic(err)
+			}
+
+			log.Printf("Created link with id: %d", id)
+			w.Write([]byte(code))
+			return
+		}
+		w.Write([]byte(link.Code))
 	}
 }
